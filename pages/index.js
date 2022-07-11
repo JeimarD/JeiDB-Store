aa03ce0a97be83aca0e490825136fb41edd5cd69
JeimarD <jeimar1215@gmail.com>
2022-07-11 12:57:20 -0400

    Modified components, added [slug] of product page, added assets, first part of the day finished

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,17 @@ import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner />
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
     <div className="products-heading">
       <h2>Best Selling Products</h2>
       <p>Speakers of many variations</p>
     </div>
     <div className="products-container">
-      {["Product 1", "Product 2"].map((product) => product)}
+      {products?.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
     </div>
-    <FooterBanner />
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </div>
 );
 
