aef251a717033308ed03f582d5153084badb0a05
JeimarD <jeimar1215@gmail.com>
2022-07-11 22:38:35 -0400

    Added context StateContext.js, added Cart functionality, fixed some style issues

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,13 @@ const Home = ({ products, bannerData }) => (
       <h2>Best Selling Products</h2>
       <p>Speakers of many variations</p>
     </div>
+
     <div className="products-container">
       {products?.map((product) => (
         <Product key={product._id} product={product} />
       ))}
     </div>
+
     <FooterBanner footerBanner={bannerData && bannerData[0]} />
   </div>
 );
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Product from "../../components/Product";
 
 import {
   AiOutlineMinus,
@@ -9,21 +10,37 @@ import {
 
 import { client, urlFor } from "../../lib/client";
 
+import { useStateContext } from "../../context/StateContext";
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+
+  const [index, setIndex] = useState(0);
+
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} alt={name} />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+              alt={name}
+            />
           </div>
-          {/* <div className="small-images-container">
-          {image?.map((item, i) => (
-            <img src={urlFor(item)} className="" onMouseEnter="" />
-          ))}
-        </div> */}
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="product-detail-desc">
@@ -44,19 +61,23 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-                0
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
@@ -68,6 +89,13 @@ const ProductDetails = ({ product, products }) => {
 
       <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
